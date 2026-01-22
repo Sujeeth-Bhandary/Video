09752977df22b59d1f0cb8638a682f7ce72dff2b
Sujeeth Bhandary <sujeethbhandari@gmail.com>
2026-01-22 17:21:15 +0530

    moogoose setUp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+dotenv.config(); // ✅ no forced path needed anymore
+
+
 import ConnectDB from './db/connection.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// ✅ FORCE correct .env file
-dotenv.config({
-  path: path.resolve(__dirname, '../.env')
-});
-
-
-
 
 ConnectDB();
